dc27f278c124cba9afe1b033785809689d45a5d9
Alina Goaga <goaga.alina@gmail.com>
2019-01-23 13:21:41 +0000

    Past events are hidden from the use (can still be found in LocalStorage)

--- src/Events.js
+++ src/Events.js
@@ -1,7 +1,24 @@
 class Events {
   constructor (events) {
     events = events || []
-    this.eventArray = events.map(function(eventPlan) { return new EventPlan(eventPlan.content, eventPlan.date, eventPlan.time)})
+
+      var i = 0
+      var removed = 0
+      var counter = 1
+      var initialLenght = events.length
+      while (counter <= initialLenght) {
+        if ( events[i].date < reformatDate(new Date().toLocaleDateString()) ) {
+           events.splice(i, 1); removed = 1
+        }
+        if (removed === 0) { i++ }
+        removed = 0
+        counter ++
+    }
+
+    this.eventArray = events.map(function(eventPlan) {
+      { return new EventPlan(eventPlan.content, eventPlan.date, eventPlan.time) }
+    })
+
   }
 
   add (eventPlan) {
