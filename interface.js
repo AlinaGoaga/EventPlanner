c319f45ad53473867349f6134b793f1de1f775e7
Alina Goaga <goaga.alina@gmail.com>
2019-01-24 22:47:08 +0000

    Get user geolocation and display weather accordingly

--- interface.js
+++ interface.js
@@ -28,9 +28,24 @@ function initialize () {
     request.open('GET', "http://api.openweathermap.org/data/2.5/weather?q=" + city.value + "&APPID=4f50ce5aeb8f1f079e6f18009dfbfbbc", true);
     request.onload = function () {
       var result = JSON.parse(this.response);
-      weatherDiv.innerText = `${result.weather[0].description} ${result.main.temp_max - 273.15}`
+      weatherDiv.innerText = `${result.weather[0].description} ; min temp: ${result.main.temp_min - 273.15}, max temp: ${result.main.temp_max - 273.15}`
     }
     request.send();
+  })
+
+  getlocationbutton.addEventListener('click', function() {
+    navigator.geolocation.getCurrentPosition(function(position) {
+     var crd = position.coords
+     var latitude = crd.latitude
+     var longitude = crd.longitude
+     var request = new XMLHttpRequest();
+     request.open('GET', `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=4f50ce5aeb8f1f079e6f18009dfbfbbc`, true);
+     request.onload = function () {
+       var result = JSON.parse(this.response);
+       weatherDiv.innerText = `${result.weather[0].description} ; min temp: ${result.main.temp_min - 273.15}, max temp: ${result.main.temp_max - 273.15}`
+     }
+     request.send();
+   });
   })
 
 }
